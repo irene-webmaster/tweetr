db48d0b4a76604efc3c79297fd8f548d7475d35b
Irene <carina.omsk@gmail.com>
2017-01-28 01:38:08 +0000

    Converted project to use SASS

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const bodyParser    = require("body-parser");
 const app           = express();
 const MongoClient   = require("mongodb").MongoClient;
 const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
+const sass          = require("node-sass");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
